103b18db08ee2c15971e43b69ac20c968f836aff
Shunji Maeda <94894616+MaedaShunji@users.noreply.github.com>
2022-03-31 23:29:05 +0700

    Lua file
    (Recommend using Visual Studio code to open it)

---
+++ Basic Level.lua
@@ -0,0 +1,10 @@
+local name = 'BasicLua' --Local is requested in all lua file
+
+function Welcome()  --Orange text is the function name you can change it if you want.
+    print('Hello')  --Print = Show the text
+    print(Apple) --This text was from line 8 if you want to make it easier you can use print("Let's eat some apple!")
+end
+
+Apple = ("Let's eat some apple!") --If you change line 5 to print("Let's eat some apple!") you can delete this line!
+
+Welcome() --Start the function if you have been change the function name **DO NOT FORGET TO CHANGE IT TOO SAME NAME WITH FUNCTION**
