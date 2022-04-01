971f4fc9917375e639649e374e46227b86cefe9c
Shunji Maeda <94894616+MaedaShunji@users.noreply.github.com>
2022-04-01 23:14:46 +0700

    Add files via upload

---
+++ PlusAndMinus.lua
@@ -0,0 +1,15 @@
+local name = 'Plus and Minus'
+
+function plus(a, b) -- Plus function.
+    print('+ Plus function with lua!')
+    return a + b
+end
+
+print(plus(8, 8))
+
+function minus(a, b) -- Minus function.
+    print('- Minus function with lua!')
+        return a - b
+    end
+
+    print(minus(6, 4))
